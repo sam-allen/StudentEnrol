03ddd518f12fabb2bcadb6c9363f55d4048b5e99
sam-allen <b5026438@my.shu.ac.uk>
2016-12-15 11:53:18 +0000

    Created Domain Class Student and Controller Class StudentController

---
+++ enrolSystem/grails-app/domain/com/enrol/Course.groovy
@@ -0,0 +1,26 @@
+package com.enrol
+
+class Course {
+		String department
+		String title
+		String leader
+		String code
+		Date startDate
+		Date endDate
+		String description
+		int numberOfStudents
+		double tuitionFees
+		String studyMode
+    static constraints = {
+	title blank:false, nullable:false
+	department blank:false, nullable:false
+	leader blank:false, nullable:false
+	code blank:false, nullable:false
+	numberOfStudents blank:false, nullable:false, minSize:20, maxSize:60
+	startDate blank:false, nullable:false
+	endDate blank:false, nullable:false
+	studyMode blank:false, nullable:false, size:1..20
+	description blank:false, nullable:false, maxSize:5000, widget:'textarea'
+	tuitionFees blank:false, nullable:false, scale:2	
+    }
+}
